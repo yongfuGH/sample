226eefb9a038b4fc3b21efa426a8ca6614f8b0ce
vagrant <vagrant@homestead>
2018-08-16 03:52:59 +0000

    Add password resets & email configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -44,5 +45,11 @@ class User extends Authenticatable
             $user->activation_token = str_random(30);
         });
     }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
+
 
 }
